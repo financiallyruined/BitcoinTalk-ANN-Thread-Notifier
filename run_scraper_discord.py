875a45a250669b41c7c7e24a771a1c46624dad6d
financiallyruined <139030112+financiallyruined@users.noreply.github.com>
2023-11-02 03:04:28 +0000

    Fixed latest thread sorting

--- run_scraper_discord.py
+++ run_scraper_discord.py
@@ -7,73 +7,60 @@ import sqlite3
 from config import *
 
 
-conn = sqlite3.connect("bitcointalk_posts.db")
-cursor = conn.cursor()
-
-cursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS new_posts (
-        msg_id TEXT PRIMARY KEY,
-        link TEXT,
-        post_topic TEXT
-    )
-"""
-)
-
-
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-BITCOINTALK_URL = "https://bitcointalk.org/index.php?board=159.0"
+BITCOINTALK_ANN_URL = "https://bitcointalk.org/index.php?board=159.0;sort=first_post;desc"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0",
 }
 
+conn = sqlite3.connect("ann_threads.db")
+cursor = conn.cursor()
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS new_threads (
+        msg_id TEXT PRIMARY KEY,
+        link TEXT,
+        thread_topic TEXT
+    )
+"""
+)
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"Discord bot connected as {bot.user.name}")
-    check_new_posts.start()
+    check_new_threads.start()
 
 
 @tasks.loop(minutes=TIME_INTERVAL)
-async def check_new_posts():
+async def check_new_threads():
     try:
-        r = requests.get(BITCOINTALK_URL, headers=headers)
-
+        r = requests.get(BITCOINTALK_ANN_URL, headers=headers)
         soup = BeautifulSoup(r.text, "html.parser")
+        # new_threads = soup.find_all("span", id=lambda value: value and value.startswith("msg"))[6:]
+        new_threads = soup.select('span[id^="msg"]')[6:]  # exclude the pinned messages
+        new_threads_info = [(row["id"], row.find("a")["href"], row.text) for row in new_threads][::-1] # reverse to make latest thread last
 
-        # new_posts = soup.find_all("span", id=lambda value: value and value.startswith("msg"))[
-        #     6:
-        # ]
+        print("Checking for new threads. Will send notification if found...")
 
-        new_posts = soup.select('span[id^="msg"]')[6:]  # exclude the pinned messages
-        new_posts_info = [
-            (row["id"], row.find("a")["href"], row.text) for row in new_posts
-        ]
+        # print(new_threads_info)
 
-        print("Checking for new posts. Will send notification if found...")
-
-        # print(new_posts_info)
-
-        cursor.execute("SELECT msg_id FROM new_posts")
+        cursor.execute("SELECT msg_id FROM new_threads")
         existing_ids = [id[0] for id in cursor.fetchall()]
-
         channel = bot.get_channel(CHANNEL_ID)
 
-        for post in new_posts_info:
-            if post[0] not in existing_ids:
-                print(f"{post[1]} - {post[2]}")
-                await channel.send(f"{post[1]} - {post[2]}")
+        for thread in new_threads_info:
+            if thread[0] not in existing_ids:
+                print(f"{thread[1]} - {thread[2]}")
+                await channel.send(f"{thread[1]} - {thread[2]}")
 
-        cursor.execute("DELETE FROM new_posts")
-
+        cursor.execute("DELETE FROM new_threads")
         cursor.executemany(
-            "INSERT INTO new_posts (msg_id, link, post_topic) VALUES (?, ?, ?)",
-            new_posts_info,
+            "INSERT INTO new_threads (msg_id, link, thread_topic) VALUES (?, ?, ?)",
+            new_threads_info,
         )
-
         conn.commit()
 
     except Exception as e:
